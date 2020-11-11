0dfc82515b7a545f42557025011d11a57a5d234a
sayitinmyface <leo99roo@gmail.com>
2020-11-11 16:34:54 +0900

    11111634

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main(){    
     cout << "Size of char : " << sizeof(char) << "\t";
     cout << "Size of int : " << sizeof(int) << "\t";
     cout << "Size of short int : " << sizeof(short int) << "\t";
-    cout << "Size of long : " << sizeof(long) << "\t";
+    cout << "Size of long : " << sizeof(long) << endl;
     cout << "Size of float : " << sizeof(float) << "\t";
     cout << "Size of double : " << sizeof(double) << "\t";
     cout << "Size of long double : " << sizeof(long double) << "\t";
     cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+    return 0;
 }
