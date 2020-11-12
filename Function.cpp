e60e2db3504bac5cc16bbe6defe4eeee9bcedea9
sayitinmyface <leo99roo@gmail.com>
2020-11-12 11:05:22 +0900

    11121105

--- Function.cpp
+++ Function.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include "under/FunctionOther.cpp"
 using namespace std;
+extern int funcOutside();
+int i = 5;
+
 // int funcInside();
 int funcInside(){
     cout << "int funcInside()"<< endl;
     return 8;
 }
+// int main(){
+//     int num = funcInside();
+//     cout << "return value :" << num << endl;
+//     return 0;
+// }
 int main(){
-    int num = funcInside();
-    cout << "return value :" << num << endl;
+    int i = funcOutside();
+    cout << "return value :" << i << endl;
+    cout << "Scope resolution operator : " << ::i << endl;
     return 0;
 }
---
+++ under/FunctionOther.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int funcOutside(){
+    cout << "int funcOutside()" << endl;
+    return 0;
+}
