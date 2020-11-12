e3930e580ec7d53ac36bbb1cd6c0b1c53c527d78
sayitinmyface <leo99roo@gmail.com>
2020-11-12 17:03:09 +0900

    11121703

--- FunctionWithParam.cpp
+++ FunctionWithParam.cpp
@@ -4,16 +4,17 @@ int max(int, int);
 int main()
 {
     int first_num, second_num, result;
-
+    // 
     cout << "first num : " << "\t";
     cin >> first_num;
     cout << "second_num : "<< "\t";
     cin >> second_num;
+    // 
     result = max(first_num, second_num);
-
+    // 
     cout << "return max value is  : "<< "\t" << result << endl;
     cout << "call only function : " << "\t" << max(second_num, first_num) << endl;
-
+    // 
     return 0;
 }
 
@@ -28,6 +29,6 @@ int max(int first_num, int second_num)
     {
         result = second_num;
     }
-
+    // 
     return result;
 }
